fabf85d7a2f44ea34fb532be00355fe24c0c3f34
Zach Deibert <zachdeibert@gmail.com>
2017-12-15 17:12:57 -0600

    Wrote OpenGL native calls for desktop platform

---
+++ Platforms/Desktop/Generator/Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using CppSharp;
+
+namespace Com.GitHub.ZachDeibert.GraphicsCore.Platforms.Desktop.Generator {
+    static class Entry {
+        static void Main(string[] args) {
+            Directory.CreateDirectory("../../../Natives");
+            ConsoleDriver.Run(new OpenGLLibrary());
+            File.Move("Com.GitHub.ZachDeibert.GraphicsCore.Platforms.Desktop.cs", "../../../Natives/OpenGL.cs");
+        }
+    }
+}
