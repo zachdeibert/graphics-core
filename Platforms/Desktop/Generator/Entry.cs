d54f6fa4bdf6edb38c537a2a39b3b4fb5bdc53f4
Zach Deibert <zachdeibert@gmail.com>
2017-12-16 13:47:43 -0600

    Wrote GLFW native calls

--- Platforms/Desktop/Generator/Entry.cs
+++ Platforms/Desktop/Generator/Entry.cs
@@ -7,7 +7,15 @@ namespace Com.GitHub.ZachDeibert.GraphicsCore.Platforms.Desktop.Generator {
         static void Main(string[] args) {
             Directory.CreateDirectory("../../../Natives");
             ConsoleDriver.Run(new OpenGLLibrary());
+            if (File.Exists("../../../Natives/OpenGL.cs")) {
+                File.Delete("../../../Natives/OpenGL.cs");
+            }
             File.Move("Com.GitHub.ZachDeibert.GraphicsCore.Platforms.Desktop.cs", "../../../Natives/OpenGL.cs");
+            ConsoleDriver.Run(new GLFWLibrary());
+            if (File.Exists("../../../Natives/GLFW.cs")) {
+                File.Delete("../../../Natives/GLFW.cs");
+            }
+            File.Move("Com.GitHub.ZachDeibert.GraphicsCore.Platforms.Desktop.cs", "../../../Natives/GLFW.cs");
         }
     }
 }
---
+++ Platforms/Desktop/Generator/GLFWLibrary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using CppSharp;
+using CppSharp.AST;
+using CppSharp.Generators;
+
+namespace Com.GitHub.ZachDeibert.GraphicsCore.Platforms.Desktop.Generator {
+    class GLFWLibrary : ILibrary {
+        public void Postprocess(Driver driver, ASTContext ctx) {
+
+        }
+
+        public void Preprocess(Driver driver, ASTContext ctx) {
+
+        }
+
+        public void Setup(Driver driver) {
+            driver.Options.GeneratorKind = GeneratorKind.CSharp;
+            Module module = driver.Options.AddModule("Com.GitHub.ZachDeibert.GraphicsCore.Platforms.Desktop");
+            File.Copy("glfw\\include\\GLFW\\glfw3.h", "GLFW.h");
+            module.IncludeDirs.Add(".");
+            module.Headers.Add("GLFW.h");
+            module.LibraryDirs.Add("glfw\\lib-vc2015");
+            module.Libraries.Add("glfw3.lib");
+        }
+
+        public void SetupPasses(Driver driver) {
+
+        }
+    }
+}
