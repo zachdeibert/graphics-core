57281045663f077adf2eab8c5f7069cb85971edd
Zach Deibert <zachdeibert@gmail.com>
2017-12-21 11:09:49 -0600

    Added OpenGL constants

--- Examples/HelloWorld/Application.cs
+++ Examples/HelloWorld/Application.cs
@@ -7,23 +7,23 @@ namespace Examples.HelloWorld {
         public string Name => "Hello World";
 
         public ApplicationType Type => ApplicationType.OpenGL;
-        IOpenGLContext GL;
+        IOpenGLContext Gl;
 
         void Render() {
-            GL.Clear(16384);
-            GL.Begin(4);
-            GL.Color3f(1, 0, 0);
-            GL.Vertex2f(0, 0);
-            GL.Color3f(0, 1, 0);
-            GL.Vertex2f(0, 1);
-            GL.Color3f(0, 0, 1);
-            GL.Vertex2f(1, 1);
-            GL.End();
+            Gl.Clear(GL.COLOR_BUFFER_BIT);
+            Gl.Begin(GL.TRIANGLES);
+            Gl.Color3f(1, 0, 0);
+            Gl.Vertex2f(0, 0);
+            Gl.Color3f(0, 1, 0);
+            Gl.Vertex2f(0, 1);
+            Gl.Color3f(0, 0, 1);
+            Gl.Vertex2f(1, 1);
+            Gl.End();
         }
 
         public void Start(IRenderContext ctx) {
-            GL = (IOpenGLContext) ctx;
-            GL.Frame += Render;
+            Gl = (IOpenGLContext) ctx;
+            Gl.Frame += Render;
         }
 
         public void Stop() {
