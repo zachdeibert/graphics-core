d179b3b6cefd0fb68f3d173b9ee76dd64b0e6975
Zach Deibert <zachdeibert@gmail.com>
2017-12-23 10:05:57 -0600

    "Fixed" desktop package again

--- Platforms/Desktop/Entry.cs
+++ Platforms/Desktop/Entry.cs
@@ -10,10 +10,10 @@ namespace Com.GitHub.ZachDeibert.GraphicsCore.Platforms.Desktop {
                 NativeLoadContext ctx = new NativeLoadContext();
                 Assembly GraphicsCore_Platforms_Desktop = ctx.LoadFromAssemblyName(typeof(Entry).GetTypeInfo().Assembly.GetName());
                 Type RenderContextFactory = GraphicsCore_Platforms_Desktop.GetType(typeof(RenderContextFactory).FullName);
-                ConstructorInfo ctor = RenderContextFactory.GetConstructor(new Type[0]);
+                ConstructorInfo ctor = RenderContextFactory.GetTypeInfo().GetConstructor(new Type[0]);
                 Assembly GraphicsCore = ctx.LoadFromAssemblyName(typeof(ApplicationSetup).GetTypeInfo().Assembly.GetName());
                 Type ApplicationSetup = GraphicsCore.GetType(typeof(ApplicationSetup).FullName);
-                MethodInfo Run = ApplicationSetup.GetMethod("Run", BindingFlags.NonPublic | BindingFlags.Static);
+                MethodInfo Run = ApplicationSetup.GetTypeInfo().GetMethod("Run", BindingFlags.NonPublic | BindingFlags.Static);
                 UriBuilder uri = new UriBuilder(GraphicsCore.CodeBase);
                 Assembly GraphicsCore_Application = ctx.LoadFromAssemblyPath(Path.Combine(Path.GetDirectoryName(Uri.UnescapeDataString(uri.Path)), "GraphicsCore.Application.dll"));
                 Run.Invoke(null, new object[] {
